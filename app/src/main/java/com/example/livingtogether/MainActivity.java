87e9b0baa799da94ee776d8345b540848a192013
Ankur Ghimire <ankurghi2341@gmail.com>
2026-02-03 09:17:47 +0545

    ag_choose mode screen

--- app/src/main/java/com/example/livingtogether/MainActivity.java
+++ app/src/main/java/com/example/livingtogether/MainActivity.java
@@ -1,26 +1,13 @@
 package com.example.livingtogether;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.livingtogether.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
@@ -30,48 +17,32 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        binding.btnBack.setOnClickListener(v -> onBackPressed());
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
+        binding.cardHaveRoom.setOnClickListener(v -> {
+            binding.checkHaveRoom.setImageResource(R.drawable.ic_check_circle);
+            binding.checkNeedRoom.setImageResource(R.drawable.ic_radio_unchecked);
+            Toast.makeText(this, "I have a room selected", Toast.LENGTH_SHORT).show();
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        binding.cardNeedRoom.setOnClickListener(v -> {
+            binding.checkNeedRoom.setImageResource(R.drawable.ic_check_circle);
+            binding.checkHaveRoom.setImageResource(R.drawable.ic_radio_unchecked);
+            Toast.makeText(this, "I need a room selected", Toast.LENGTH_SHORT).show();
+        });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                return true;
+            } else if (id == R.id.nav_matches) {
+                return true;
+            } else if (id == R.id.nav_messages) {
+                return true;
+            } else if (id == R.id.nav_profile) {
+                return true;
+            }
+            return false;
+        });
     }
 }
